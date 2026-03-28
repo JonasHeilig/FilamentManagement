b9bc8e146b859999364c2c0f4aaa4acf148cf1d7
JonasHeilig <jonas.maximilian@die-heiligs.de>
2026-03-28 01:29:09 +0100

    vercel demo2

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ def create_app(test_config=None):
     app = Flask(__name__, template_folder='templates')
     app.config.setdefault('SQLALCHEMY_DATABASE_URI', 'sqlite:///spools.db')
     app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+
     if test_config:
         app.config.update(test_config)
+
     db.init_app(app)
     app.register_blueprint(api_bp)
-
-    with app.app_context():
-        db.create_all()
 
     @app.route('/')
     def index():
@@ -23,9 +22,7 @@ def create_app(test_config=None):
 
 
 if __name__ == '__main__':
+    app = create_app()
     with app.app_context():
         db.create_all()
-    app = create_app()
-    app.run()
-
-app = create_app()
+    app.run(debug=True)
